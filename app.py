3d865adb6719e2c545e3fc72b18ed046da5ef86b
hiteshproy@gmail.com <hiteshproy@gmail.com>
2019-01-04 11:30:53 +0530

    updated form html

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_cors import CORS
 app = Flask(__name__)
 
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    print(request.form)
+    """ text = request.form['text']
+    print(text) """
+    """ processed_text = text.upper()
+    return processed_text """
 
 if __name__ == '__main__':
     app.run()
